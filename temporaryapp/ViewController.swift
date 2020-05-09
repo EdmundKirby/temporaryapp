c686824396e5eb901c6147eb24d681842fbb6f31
andrewmcgee <ac.mcgee@outlook.com>
2020-05-09 12:44:59 +0100

    Added changing color ability

--- temporaryapp/ViewController.swift
+++ temporaryapp/ViewController.swift
@@ -16,8 +16,23 @@ class ViewController: UIViewController {
         let label = UILabel(frame: rect)
         label.text = "Hi There!"
         view.addSubview(label)
+        
+        let buttonRect = CGRect(x: 50, y: 200, width: 200, height: 50)
+        let button = UIButton(frame: buttonRect)
+        button.backgroundColor = UIColor.blue
+        button.layer.cornerRadius = 5
+        button.setTitle("Change color!", for: .normal)
+        button.addTarget(self, action: #selector(changeBackgroundColor), for: .touchUpInside)
+        view.addSubview(button)
     }
 
-
+    @objc func changeBackgroundColor() {
+        let red = CGFloat.random(in: 0...255)/256
+        let green = CGFloat.random(in: 0...255)/256
+        let blue = CGFloat.random(in: 0...255)/256
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        view.backgroundColor = color
+    }
+    
 }
 
